7cf896b077dcd72f3d9e3a5185fc886153d06b65
m-zaremba <zaremba_michal@wp.pl>
2019-12-13 19:15:05 +0100

    axios setup update

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,31 +1,76 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button, TouchableOpacity } from 'react-native';
 import { ThemeContext, activeTheme, withTheme } from '../contexts/ThemeContext';
 import { themes, testThemes } from '../themes/themes';
+import Icon from 'react-native-vector-icons/Ionicons';
 import axios from 'axios';
 
 const Home = ({themes, activeTheme}) => {
   const { colors, setColors, toggleTheme } = useContext(ThemeContext);
+  const [data, setData] = useState({});
+  const [searchValue, setSearchValue] = useState('');
+  const [searchMovie, setSearchMovie] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-  const [data, setData] = useState();
+  const resetInputField = () => {
+    setSearchValue("")
+  }
+
+  const handleSearch = () => {
+    setSearchMovie(searchValue);
+    resetInputField();
+  }
+
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
-        'http://www.omdbapi.com/?t=the+mask&plot=full&apikey=f1c551f9&?',
-      );
-      setData(result.data);
+      setError(false);
+      setLoading(true);
+
+      try {
+        const result = await axios(
+        `http://www.omdbapi.com/?t=${searchMovie}&plot=full&apikey=f1c551f9&?`,
+        );
+        setData(result.data);
+      } catch (error) {
+        setError(true);
+      }
+      setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [searchMovie]);
 
-  console.log(data);
 
   return (
-    <>
+
       <View style={{...styles.mainView, backgroundColor: activeTheme.backgroundColor }}>
-        <Text style={{...styles.mainText, color: activeTheme.color}}>Hello Native World</Text>
+        <View style={styles.searchBar}>
+          <TouchableOpacity
+            onPress={() => {handleSearch()}}
+          >
+            <Icon name='ios-search' size={30} color='red' />
+          </TouchableOpacity>
+          <TextInput
+            style={styles.inputText}
+            autoCapitalize='none'
+            placeholder='Enter movie title'
+            onChangeText={value => setSearchValue(value)}
+            value={searchValue}
+          />
+        </View>
+
+
+        {error && <Text>Ups... Something didn't go according to the plan :(</Text>}
+
+        {(searchMovie === '') ? null : loading ? (
+          <Text style={{...styles.mainText, color: activeTheme.color}}>Searching...</Text>
+        ) : data.Title ? (
+          <Text style={{...styles.mainText, color: activeTheme.color}}>
+            {data.Plot}
+          </Text>
+        ) : <Text style={{...styles.mainText, color: activeTheme.color}}>Sorry - no such movie (or wrong title)</Text>}
       </View>
-    </>
+
   )
 }
 
@@ -39,7 +84,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   mainText: {
-    fontSize: 40,
-    fontWeight: 'bold',
+    fontSize: 20,
   },
+  searchBar: {
+    paddingTop: 25,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  inputText: {
+    color: 'black',
+    height: 40,
+    width: '70%',
+    marginTop: 8,
+    backgroundColor: 'rgba(255, 255, 255, 0.92)',
+    paddingLeft: 20,
+    borderBottomWidth: 2,
+    borderBottomColor: 'rgb(245, 71, 71)'
+  }
 });
