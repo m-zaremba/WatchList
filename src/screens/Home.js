cf1862ecbc3b23d4fd46a32f5d31018dba38076c
m-zaremba <zaremba_michal@wp.pl>
2019-12-14 17:03:59 +0100

    axios and style update

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { View, Text, TextInput, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native';
 import { ThemeContext, activeTheme, withTheme } from '../contexts/ThemeContext';
 import { themes, testThemes } from '../themes/themes';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -14,7 +14,7 @@ const Home = ({themes, activeTheme}) => {
   const [error, setError] = useState(false);
 
   const resetInputField = () => {
-    setSearchValue("")
+    setSearchValue('')
   }
 
   const handleSearch = () => {
@@ -31,7 +31,7 @@ const Home = ({themes, activeTheme}) => {
         const result = await axios(
         `http://www.omdbapi.com/?t=${searchMovie}&plot=full&apikey=f1c551f9&?`,
         );
-        setData(result.data);
+        searchMovie !== '' ? setData(result.data) : setData({});
       } catch (error) {
         setError(true);
       }
@@ -40,35 +40,68 @@ const Home = ({themes, activeTheme}) => {
     fetchData();
   }, [searchMovie]);
 
+  console.log(data);
 
   return (
 
       <View style={{...styles.mainView, backgroundColor: activeTheme.backgroundColor }}>
-        <View style={styles.searchBar}>
+        <View style={{...styles.searchBar, color:activeTheme.color}}>
           <TouchableOpacity
+            style={styles.searchButton}
             onPress={() => {handleSearch()}}
           >
-            <Icon name='ios-search' size={30} color='red' />
+            <Icon name='ios-search' size={30} color={activeTheme.color} />
           </TouchableOpacity>
           <TextInput
-            style={styles.inputText}
+            style={{...styles.inputText, color:activeTheme.color}}
             autoCapitalize='none'
             placeholder='Enter movie title'
+            placeholderTextColor={activeTheme.color}
             onChangeText={value => setSearchValue(value)}
             value={searchValue}
           />
         </View>
 
-
         {error && <Text>Ups... Something didn't go according to the plan :(</Text>}
 
-        {(searchMovie === '') ? null : loading ? (
-          <Text style={{...styles.mainText, color: activeTheme.color}}>Searching...</Text>
-        ) : data.Title ? (
-          <Text style={{...styles.mainText, color: activeTheme.color}}>
-            {data.Plot}
-          </Text>
-        ) : <Text style={{...styles.mainText, color: activeTheme.color}}>Sorry - no such movie (or wrong title)</Text>}
+        {data.Error && <Text style={{...styles.mainText, color: activeTheme.color}}>
+          Sorry - no such movie (or wrong title)
+        </Text>}
+
+        {(searchMovie === '') ? null
+        :
+          loading ?
+            (
+              <Text style={{...styles.mainText, color: activeTheme.color}}>Searching...</Text>
+            )
+          :
+          data.Title &&
+          (
+            <ScrollView contentContainerStyle={styles.movieDetails}>
+              {/* <Text style={{...styles.title, color: activeTheme.color}}>{data.Title}</Text> */}
+              <Image
+                style={styles.poster}
+                source={{uri: data.Poster}}
+                resizeMode="contain"
+              />
+              <View style={styles.stats}>
+                <Text style={{...styles.mainText, color: activeTheme.color, flex: 1}}>Duration:{'\n'} {data.Runtime}</Text>
+                <Text style={{...styles.mainText, color: activeTheme.color, flex: 1}}>Genre:{'\n'} {data.Genre}</Text>
+              </View>
+              {data.Ratings && <View style={styles.ratingsWrapper}>
+                <Text>Ratings:{'\n'}</Text>
+                {data.Ratings.map(e => {
+                  return <Text key={e.Source} style={{...styles.movieRating, color: activeTheme.color}}>{e.Source + ': ' + e.Value}</Text>
+                })}
+              </View>}
+              <Text style={{...styles.mainText, color: activeTheme.color}}>Director: {data.Director}</Text>
+              <Text style={{...styles.mainText, color: activeTheme.color}}>Main cast:{'\n'}{data.Actors}</Text>
+              <Text style={{...styles.moviePlot, color: activeTheme.color}}>Plot:{'\n'}
+                {data.Plot}
+              </Text>
+            </ScrollView>
+          )
+        }
       </View>
 
   )
@@ -81,27 +114,59 @@ const styles = StyleSheet.create({
     paddingTop: 40,
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
   mainText: {
     fontSize: 20,
+    lineHeight: 30,
+    marginBottom: 20
   },
   searchBar: {
-    paddingTop: 25,
-    position: 'absolute',
-    top: 0,
-    left: 0,
     display: 'flex',
-    flexDirection: 'row'
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   inputText: {
-    color: 'black',
     height: 40,
-    width: '70%',
-    marginTop: 8,
-    backgroundColor: 'rgba(255, 255, 255, 0.92)',
+    width: '90%',
     paddingLeft: 20,
-    borderBottomWidth: 2,
-    borderBottomColor: 'rgb(245, 71, 71)'
+  },
+  searchButton: {
+    height: 40,
+    width: '10%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  movieDetails: {
+    padding: 20,
+    justifyContent: 'center',
+    marginBottom: 40
+  },
+  title: {
+    fontSize: 35,
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    alignSelf: 'center'
+  },
+  ratingsWrapper: {
+    marginTop: 15,
+    marginBottom: 15
+  },
+  movieRating: {
+    fontSize: 20,
+    lineHeight: 25,
+  },
+  moviePlot: {
+    fontSize: 20,
+    lineHeight: 30,
+    textAlign: 'justify'
+  },
+  poster: {
+    height: 500,
+    width: '100%',
+    alignSelf: 'center'
+  },
+  stats: {
+    flexDirection: 'row',
   }
 });
